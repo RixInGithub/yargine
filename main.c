c90c3f09b87b5581b48f4ef0ca43d1d0ba62cc1a
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2025-12-20 19:23:21 +0200

    added functions to actually create and edit ystrs

--- main.c
+++ main.c
@@ -103,7 +103,7 @@ bool initYarg(c*name) {
 	return true;
 }
 
-bool readYarg() {
+bool readYarg() { // despite it's name, also checks ystr.bin now!
 	c full[PATH_MAX] = {0};
 	snprintf(full, sizeof(full), "%s/%s", dir, "yarg.bin");
 	FILE*ygFile = fopen(full,"rb");
@@ -111,20 +111,30 @@ bool readYarg() {
 	size_t amnt = fread(&thisProj, sizeof(thisProj), 1, ygFile);
 	fclose(ygFile);
 	if (amnt!=1) return false;
-	return strncmp(thisProj.hdr,"YARG",4)==0;
+	yHdr ystrHdr;
+	memset(full,0,sizeof(full));
+	snprintf(full, sizeof(full), "%s/%s", dir, "ystr.bin");
+	FILE*yrFile = fopen(full,"rb");
+	if (!(yrFile)) return false;
+	if (fread(ystrHdr, 4, 1, yrFile)!=1) {
+		fclose(yrFile);
+		return false;
+	}
+	fclose(yrFile);
+	return ((strncmp(thisProj.hdr,"YARG",4)==0)&&(strncmp(ystrHdr,"YSTR",4)==0));
 }
 
 c*readYstr(ystrIdx idx) {
 	if (idx==0) return calloc(1,1); // null terminated blank string
 	uint32_t strSize;
 	ystrIdx zeroBased = idx-1;
-	int count = 0;
+	ystrIdx count = 0;
 	c full[PATH_MAX] = {0};
 	snprintf(full, sizeof(full), "%s/%s", dir, "ystr.bin");
 	FILE*yrFile = fopen(full,"rb");
 	if (!(yrFile)) return NULL;
 	yHdr ystrHdr;
-	if (fread(&ystrHdr, sizeof(yHdr), 1, yrFile)!=1) {
+	if (fread(ystrHdr, sizeof(yHdr), 1, yrFile)!=1) {
 		fclose(yrFile);
 		return NULL; // tehe segfault~ >:3
 	}
@@ -158,6 +168,137 @@ c*readYstr(ystrIdx idx) {
 	}
 	fclose(yrFile);
 	return res;
+}
+
+ystrIdx genYstr(c*new) {
+	if (!(new)) return 0;
+	if (*new==0) return 0;
+	uint32_t strSize;
+	// ystr indices always start from 1
+	ystrIdx count = 0;
+	c full[PATH_MAX] = {0};
+	snprintf(full, sizeof(full), "%s/%s", dir, "ystr.bin");
+	FILE*yrFile = fopen(full,"rb");
+	if (!(yrFile)) return 0;
+	yHdr ystrHdr;
+	if (fread(ystrHdr, sizeof(yHdr), 1, yrFile)!=1) {
+		fclose(yrFile);
+		return 0;
+	}
+	if (strncmp(ystrHdr,"YSTR",4)!=0) {
+		fclose(yrFile);
+		return 0;
+	}
+	while (!(feof(yrFile))) {
+		if (fread(&strSize, sizeof(uint32_t), 1, yrFile)!=1) {
+			fclose(yrFile);
+			return 0;
+		}
+		if (fseek(yrFile, strSize, SEEK_CUR)!=0) { // 0 reads here...
+			fclose(yrFile);
+			return 0;
+		}
+		count++; // :p
+	}
+	fclose(yrFile);
+	yrFile = fopen(full,"ab");
+	uint32_t lengthOfTS = (uint32_t)strlen(new); // snatched from modifyYstr lmao
+	fwrite(&lengthOfTS, sizeof(uint32_t), 1, yrFile);
+	fwrite(new, lengthOfTS, 1, yrFile);
+	fclose(yrFile);
+	return count+1;
+}
+
+bool modifyYstr(ystrIdx idx, c*new) {
+	if (idx==0) return false;
+	uint32_t strSize;
+	ystrIdx zeroBased = idx-1;
+	c*beforeIdx=calloc(sizeof(yHdr)+1,1); // hehy null term
+	if (!(beforeIdx)) return false;
+	ystrIdx count = 0;
+	c full[PATH_MAX] = {0};
+	snprintf(full, sizeof(full), "%s/%s", dir, "ystr.bin");
+	FILE*yrFile = fopen(full,"rb");
+	if (!(yrFile)) return false;
+	if (fread(beforeIdx, sizeof(yHdr), 1, yrFile)!=1) {
+		fclose(yrFile);
+		return false;
+	}
+	if (strncmp(beforeIdx,"YSTR",4)!=0) {
+		fclose(yrFile);
+		return false;
+	}
+	while (count<zeroBased) {
+		if (fread(&strSize, sizeof(uint32_t), 1, yrFile)!=1) {
+			fclose(yrFile);
+			free(beforeIdx);
+			return false;
+		}
+		c*add = calloc(sizeof(c),strSize+1);
+		if (!(add)) {
+			fclose(yrFile);
+			free(beforeIdx);
+		}
+		if (fread(add, strSize, 1, yrFile)!=1) {
+			fclose(yrFile);
+			free(beforeIdx);
+			free(add);
+			return false;
+		}
+		size_t prevSize = strlen(beforeIdx);
+		beforeIdx = realloc(beforeIdx,prevSize+sizeof(uint32_t)+strSize+1); // null term
+		if (!(beforeIdx)) { // YLTPYL PART 1 (TRY NOT TO LEAK MEMORY!!!)
+			fclose(yrFile);
+			free(add);
+			return false;
+		}
+		memcpy(beforeIdx+prevSize, &strSize, sizeof(uint32_t));
+		memcpy(beforeIdx+prevSize+sizeof(uint32_t), add, strSize);
+		beforeIdx[prevSize+sizeof(uint32_t)+strSize]=0;
+		free(add);
+		count++;
+	}
+	if (fread(&strSize, sizeof(uint32_t), 1, yrFile)!=1) {
+		fclose(yrFile);
+		free(beforeIdx);
+		return false;
+	}
+	if (fseek(yrFile, strSize, SEEK_CUR)!=0) {
+		fclose(yrFile);
+		free(beforeIdx);
+		return false;
+	}
+	long int curr = ftell(yrFile);
+	fseek(yrFile, 0, SEEK_END);
+	long int end = ftell(yrFile);
+	fseek(yrFile, curr, SEEK_SET); // get yo goofy ahh back here
+	size_t remaining = end - curr;
+	c*afterIdx = calloc(sizeof(c),remaining+1);
+	if (!(afterIdx)) {
+		fclose(yrFile);
+		free(beforeIdx);
+		return false;
+	}
+	if (fread(afterIdx, remaining, 1, yrFile)!=1) {
+		fclose(yrFile);
+		free(beforeIdx);
+		free(afterIdx);
+		return false;
+	}
+	fclose(yrFile);
+	yrFile = fopen(full,"wb");
+	// get ready for satisfying freeing...
+	fwrite(beforeIdx, strlen(beforeIdx), 1, yrFile);
+	free(beforeIdx); // mmhh
+	// WE INTERRUPT THIS PROGRAM WITH AN INBETWEEN WRITING WITH NO MALLOCS OR FREEING!!!
+	uint32_t lengthOfTS = (uint32_t)strlen(new);
+	fwrite(&lengthOfTS, sizeof(uint32_t), 1, yrFile);
+	fwrite(new, lengthOfTS, 1, yrFile);
+	// we will now go back to our regular programming...
+	fwrite(afterIdx, remaining, 1, yrFile);
+	free(afterIdx); // "so gooood..." - the literal system
+	fclose(yrFile); // "do i even deserve this..." - system
+	return true; // thank for watching like and subcrib
 }
 
 bool isDir(c*p) {
@@ -390,7 +531,9 @@ int main(int argc, char**argv) {
 						if (renderM!=NONYARGWARN) break;
 						needsRender = true;
 						if (initCh==2) {
-							dir = dirB4Enter;
+							free(dir);
+							dir = calloc(strlen(dirB4Enter)+1,sizeof(c));
+							strcpy(dir,dirB4Enter);
 							setupDirCnsts();
 							break;
 						}
