dd2d9a3ab185218e6efa0f3496a013db43e811cc
RixInGithub <87478581+RixInGithub@users.noreply.github.com>
2026-01-03 18:35:44 +0200

    continue lyarg's development

--- lyarg.lua
+++ lyarg.lua
@@ -1,8 +1,9 @@
 #!/usr/bin/env lua
--- i chose lua bcuz i think its just enough higher level than c to not make me lose my mind
--- in lyarg, whitespace is optional unless there's no other way around it.
--- btw _ is whitespace cuz its barely blank (cry about it)
--- so instead of |+ 1 1| you can do |+_1_1| (fuck them snake case ppl)
+--[[ i chose lua bcuz i think its just enough higher level than c to not make me lose my mind
+	 in lyarg, whitespace is optional unless there's no other way around it.
+	 btw _ is whitespace cuz its barely blank (cry about it)
+	 so instead of |+ 1 1| you can do |+_1_1| (fuck them snake case ppl) ]]
+parsed = {}
 inpIO = io.open(arg[1]or"./main.yrg")
 inp = inpIO:read("*a")
 inpIO:close()
@@ -28,12 +29,61 @@ function expressionParse()
 		__inp=__inp:sub(#out+1)
 		return out
 	end
-	return ""
+	if __inp:sub(1,11)=="nonexistent" then
+		out = "nonexistent"
+		__inp=__inp:sub(#out+1)
+		return out
+	end
+	local pInp = __inp:sub(1)
+	local isId, id = pcall(slurpUntilWS, "id")
+	if not isId then __inp = pInp end
+	if isId then return id end
+	if __inp:sub(1,1) == "[" then
+		out = "["
+		__inp = __inp:sub(2)
+		while __inp:sub(1,1) ~= "]" do
+			slurpWS()
+			if __inp:sub(1,1) == "{" then
+				__inp = __inp:sub(2)
+				slurpWS()
+				if __inp:sub(1,1) ~= "[" then error("magic knife that splits arrays to nonarrays? howz?") end
+				local firstLn = expressionParse()
+				slurpWS()
+				if __inp:sub(1,1) ~= "," then error("blunt knife") end
+				__inp = __inp:sub(2)
+				slurpWS()
+				if __inp:sub(1,1) ~= "[" then error("magic knife that splits arrays to nonarrays? howz?") end
+				local sndLn = expressionParse()
+				slurpWS()
+				if __inp:sub(1,1) ~= "}" then error("expected }") end
+				__inp = __inp:sub(2)
+				slurpWS()
+				if __inp:sub(1,1) ~= "]" then error("expected ]") end
+				__inp = __inp:sub(2)
+				return out:sub(1,-2) .. "]>" .. firstLn .. ">" .. sndLn
+			end
+			out = out..expressionParse()
+			slurpWS()
+			if __inp:sub(1,1) ~= "]" then
+				if __inp:sub(1,1) ~= "," then error("expected ,") end
+				__inp = __inp:sub(2)
+			end
+			out = out..","
+		end
+		__inp = __inp:sub(2)
+		return out:sub(1,-2) .. "]"
+	end
+	print(__inp:sub(1,1))
+	error("expected expressionoso")
+end
+
+function addToLatestParse(txt)
+	parsed[#parsed] = parsed[#parsed]..#txt..">"..txt
 end
 
 function isWS(c)
 	if #c~=1 then return false end
-	return string.find("\x20\t\r\n_",c,1,true)~=nil -- accurate c translation of doing this: |strrchar("...",c)!=NULL|
+	return string.find("\x20\t\r\n_",c,1,true)~=nil -- accurate c translation of doing this: |strchr("...",c)!=NULL|
 end
 
 function slurpUntilWS(rule)
@@ -41,12 +91,12 @@ function slurpUntilWS(rule)
 	while (not isWS(__inp:sub(1,1))) and #__inp>0 do
 		ch = __inp:sub(1,1)
 		if rule == "id" and ch:find("[%a@#$]")==nil then
-			error("ye folk expected an identifier without a foul character in the name")
+			error("ye folk expected a name without a foul character inside")
 		end
 		out = out .. ch -- if only it was that simple in c...
 		__inp = __inp:sub(2) -- stare at this instead, this is easy in c. it's just |__inp++;|. look at it, the beauty hides within.
 	end
-	if rule=="id" and (out=="yar" or out=="yarg" or out=="f") then error("ye folk know the name from the legend of the keywords, is it really them? the keyword gods?") end -- populate with more keywords
+	if rule=="id" and (out=="yar" or out=="yarg" or out=="f" or out=="nonexistent") then error("ye folk know the name from the legend of the keywords, is it really them? the keyword gods?") end -- populate with more keywords
 	-- if rule=="id" and out == "_specialStackStart" then out = "_"..out end -- actually go shit yourselves
 	-- anyways day 4 of saving overhead every day™ (trust)
 	return out
@@ -70,13 +120,12 @@ function slurpComment()
 	slurpWS()
 end
 
--- these will be simple c**s in c:
 local yargTbl = {}
 local yarTbl = {}
 local fTbl = {}
-parsed = {}
 if __inp:sub(1,2) == "#!" then slurpComment() end -- shebang (why not)
 while #__inp>0 do
+	slurpWS()
 	while __inp:sub(1,3) == "..." do -- day 3 of saving overhead every day™ (trust)
 		slurpComment()
 	end
@@ -114,9 +163,24 @@ while #__inp>0 do
 		slurpWS()
 		table.insert(varTbl,id)
 		local exp = expressionParse() -- |lua -e "print(exp==nil)"| => |true| on my machine™ why it ourple
-		if exp=="" then error("expected expressionoso") end
-		table.insert(parsed,#exp..":"..exp)
-		print("added a var!")
+		table.insert(parsed,"")
+		addToLatestParse("yar"..(isConst and "g" or ""))
+		addToLatestParse(id)
+		addToLatestParse(exp)
+		if isConst then
+			slurpWS()
+			if __inp:sub(1,19) == "... shut up yargine" then
+				if string.find("\r\n",__inp:sub(20,20),1,true) ~= nil then
+					slurpComment()
+					goto lyarg_continue0
+				end
+			end
+			print(string.format("be warned, it's too late to yar %s now!",id))
+		end
+		goto lyarg_continue0
 	end
+	print("before unknown char", __inp:sub(1))
+	error(string.format("unknown character 0x%x", __inp:byte(1)))
+	::lyarg_continue0::
 end
-print(require("dkjson").encode(parsed))
+print(table.concat(parsed,"\n"))
